2f1e81883a370e4ac26f1c943bfa57bc8c38f5f9
alex0blackwell <alex.blackwell314@gmail.com>
2020-06-06 17:39:17 -0700

    Now in true colour! Point system added

--- flappyBird.py
+++ flappyBird.py
@@ -3,11 +3,16 @@ import time, random, os, keyboard
 # install keyboard, needs sudo
 
 w, h = 100, 10;
-gameBoard = [[0 for x in range(w)] for y in range(h)]
+firstPipePos = score = 0;
 birdYPos = h//2
-firstPipePos = 0;
 pipeSplitsQueue = []
+gameBoard = [[0 for x in range(w)] for y in range(h)]
 collision = False
+
+
+def rbgToAnsii(word, r, g, b):
+    return f"\x1b[38;2;{r};{g};{b}m{word}\x1b[0m"
+
 
 class Game():
     """docstring for Game."""
@@ -17,16 +22,16 @@ class Game():
 
         for i in range(h):
             if(not(split-3 <= i <= split)):
-                gameBoard[i][xpos] = '@'
+                gameBoard[i][xpos] = rbgToAnsii('@', 116, 191, 46)
                 if(xpos+1 < w):
-                    gameBoard[i][xpos+1] = '@'
+                    gameBoard[i][xpos+1] = rbgToAnsii('@', 116, 191, 46)
                     if(xpos+2 < w):
-                        gameBoard[i][xpos+2] = f"{'-' if (i < 8) else '='}"
+                        gameBoard[i][xpos+2] = f"{rbgToAnsii('-', 113, 197, 207) if (i < 8) else rbgToAnsii('=', 221, 216, 148)}"
 
     def deletePipe(self, xpos):
         for i in range(h):
-            gameBoard[i][xpos] = f"{'-' if (i < 8) else '='}"
-            gameBoard[i][xpos+1] = f"{'-' if (i < 8) else '='}"
+            gameBoard[i][xpos] = f"{rbgToAnsii('-', 113, 197, 207) if (i < 8) else rbgToAnsii('=', 221, 216, 148)}"
+            gameBoard[i][xpos+1] = f"{rbgToAnsii('-', 113, 197, 207) if (i < 8) else rbgToAnsii('=', 221, 216, 148)}"
 
     def startGame(self):
 
@@ -34,7 +39,7 @@ class Game():
         pipeSplitsQueue = []
         for i in range(h):
             for j in range(w):
-                gameBoard[i][j] = f"{'-' if (i < 8) else '='}"
+                gameBoard[i][j] = f"{rbgToAnsii('-', 113, 197, 207) if (i < 8) else rbgToAnsii('=', 221, 216, 148)}"
         # spawn 4 pipes every
         firstPipePos = 15
         for i in range(15, 100, 15):
@@ -45,13 +50,22 @@ class Game():
 
     def printFrame(self):
         for i in range(h):
-            print(''.join(gameBoard[i]))
+            res = ""
+            for j in range(w):
+                # print('\033[07m', end='')  # reverse colours
+                res += '\033[07m'+gameBoard[i][j]
+            print(res)
 
     def updatePipes(self, frames):
         '''update pipes every 3th frame, spawn in new pipes, spawn out old pipes'''
         if(frames % 3 == 0):
+            global score
             global firstPipePos
             firstPipePos -= 1
+
+            if(firstPipePos == 5):
+                score += 1
+
             if(firstPipePos < 0):
                 # this pipe must get removed
                 thisPipe = Game()
@@ -75,38 +89,42 @@ class Game():
 class Bird():
     """docstring for Bird."""
 
-
     def drawBird(self, x, y):
         '''note the bird requires the space above it'''
         global birdYPos
         birdYPos = y
         # first clear the bird before
+        body = rbgToAnsii('#', 249, 241, 36)
+        front = rbgToAnsii('\\', 249, 241, 36)
+        beak = rbgToAnsii('>', 250, 103, 75)
         for i in range(h):
             for j in range(x, x+3):
                 coor = gameBoard[i][j]
-                if(coor == '#' or coor == '\\' or coor == '>'):
-                    gameBoard[i][j] = f"{'-' if (i < 8) else '='}"
+                if(coor == body or coor == front or coor == beak):
+                    gameBoard[i][j] = f"{rbgToAnsii('-', 113, 197, 207) if (i < 8) else rbgToAnsii('=', 221, 216, 148)}"
 
         # check for collision on this bird
-        top = gameBoard[y-1][x]
-        topR = gameBoard[y-1][x+1]
+        # top = gameBoard[y-1][x]
+        # topR = gameBoard[y-1][x+1]
         coor = gameBoard[y][x]
         right = gameBoard[y][x+1]
         twoR = gameBoard[y][x+2]
+        pole = rbgToAnsii('@', 116, 191, 46)
 
-        if(top == '@' or topR == '@' or coor == '@' or right == '@' or twoR == '@'):
+        if(coor == pole or right == pole or twoR == pole):
             global collision
             collision = True
 
-        gameBoard[y-1][x] = '#'  # above
-        gameBoard[y-1][x+1] = '\\'  # above right
-        gameBoard[y][x] = '#'  # at coordinate
-        gameBoard[y][x+1] = '#'  # right
-        gameBoard[y][x+2] = '>'  # two right
+        mainBody = rbgToAnsii('#', 249, 241, 36)
+        # gameBoard[y-1][x] = mainBody # above
+        gameBoard[y][x] = mainBody  # at coordinate
+        gameBoard[y][x+1] = mainBody  # right
+        # gameBoard[y-1][x+1] = rbgToAnsii('\\', 249, 241, 36) # above'\\'  # above right
+        gameBoard[y][x+2] = rbgToAnsii('>', 250, 103, 75)  # two right
 
 
 
-    def gravity(self):
+    def gravity(self):# \x1b[38;2;{r};{g};{b}m{darkToBright[totalBrightness//70]}\x1b[0m
         '''drop the bird by a row'''
         dropBy = 1
         if(birdYPos+dropBy >= h):
@@ -129,51 +147,56 @@ class Bird():
 
 def main():
 
-    global collision
-    jumped = endGame = False
+    global collision, score
+    jumped = endGame = scoreUpdated = False
     frames = 1
     frameTime = 0.2  # time for each frame
     while(not endGame):
+        score = 0
         game = Game()
         bird = Bird()
         game.startGame()
+        bird.drawBird(5, birdYPos)
         while(not collision and not endGame):
+            scoreUpdated = False
             os.system('cls' if os.name == 'nt' else 'clear')
             if(not jumped):
                 bird.gravity()
-            else:
-                bird.jump()
-            game.updatePipes(frames)
 
+            game.updatePipes(frames)
             game.printFrame()
 
             frames += 1
             if(frames == 1000):
-                # in case of overflow
+                # to rid overflow potential
                 frames = 1
-            print(frames)
 
-            print("Press \"e\" to end the game")
+            print(f"Your score is {score}!\n")
+
+            print("Press \"e\" to end the round")
             print("Press space to jump! ")
             t0 = time.time()
             jumped = False
             while(time.time()-t0 < frameTime):
                 if keyboard.is_pressed(' '):
                     jumped = True;
+                    bird.jump()
                     break;
                 elif keyboard.is_pressed('e'):
                     endGame = True
+                    break
 
             wait = round(time.time()-t0, 2)
-            # rounding errors if we dont round
+            # floating point errors if we dont round
             time.sleep(frameTime-wait)
 
         if(collision):
-            print("whoops, you had a collision!")
-        userIn = input("Type \"end\" to end the game, press \"enter\" to play again:\n")
+            print("\nWhoops, you had a collision!")
+        userIn = input("\nType \"end\" to end the game, press \"enter\" to play again:\n")
         if("end" in userIn):
             endGame = True
-        collision = False
+        else:
+            collision = endGame = False
 
 
 if __name__ == '__main__':
